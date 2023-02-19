651f209265d16d5f4987c8aea7b39ac18d75bd1e
AKORA <dragoncatm@gmail.com>
2023-02-19 19:05:48 +0100

    Added view uniform

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use log::info;
+use wgpu::util::DeviceExt;
 use winit::{
     event::*,
     event_loop::{ControlFlow, EventLoop},
@@ -16,7 +17,47 @@ struct State {
     window: Window,
     render_pipeline: wgpu::RenderPipeline,
     #[allow(dead_code)]
-    clear_color: wgpu::Color,
+    view_uniform: ViewUniform,
+    #[allow(dead_code)]
+    view_buffer: wgpu::Buffer,
+    view_bind_group: wgpu::BindGroup,
+}
+
+// We need this for Rust to store our data correctly for the shaders
+#[repr(C)]
+// This is so we can store this in a buffer
+#[derive(Debug, Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
+struct ViewUniform {
+    dpi: f32,
+    radius: f32,
+    max_iterations: u32,
+    /// Point at the bottom left point of the screen
+    // bot_left: [f32; 2],
+    /// Vector pointing to the top right of the screen
+    // screen_vec: [f32; 2],
+    x_min: f32,
+    x_max: f32,
+    y_min: f32,
+    y_max: f32,
+}
+
+impl ViewUniform {
+    pub fn new() -> Self {
+        // X Range
+        let x_r = [-2.00, 0.47];
+        // Y Range
+        let y_r = [-1.12, 1.12];
+
+        Self {
+            dpi: 500.,
+            radius: 2.,
+            max_iterations: 500,
+            x_min: x_r[0],
+            x_max: x_r[1],
+            y_min: y_r[0],
+            y_max: y_r[1],
+        }
+    }
 }
 
 impl State {
@@ -98,13 +139,6 @@ impl State {
         };
         surface.configure(&device, &config);
 
-        let clear_color = wgpu::Color {
-            r: 0.1,
-            g: 0.2,
-            b: 0.3,
-            a: 1.0,
-        };
-
         // Render pipeline
 
         let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
@@ -112,10 +146,42 @@ impl State {
             source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
         });
 
+        // View Unifrom
+        let view_uniform = ViewUniform::new();
+        let view_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("View Uniform Buffer"),
+            contents: bytemuck::cast_slice(&[view_uniform]),
+            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+        });
+
+        let view_bind_group_layout =
+            device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+                entries: &[wgpu::BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: wgpu::ShaderStages::FRAGMENT,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                    count: None,
+                }],
+                label: Some("view_bind_group_layout"),
+            });
+
+        let view_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            layout: &view_bind_group_layout,
+            entries: &[wgpu::BindGroupEntry {
+                binding: 0,
+                resource: view_buffer.as_entire_binding(),
+            }],
+            label: Some("camera_bind_group"),
+        });
+
         let render_pipeline_layout =
             device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
                 label: Some("Render Pipeline Layout"),
-                bind_group_layouts: &[],
+                bind_group_layouts: &[&view_bind_group_layout],
                 push_constant_ranges: &[],
             });
 
@@ -167,7 +233,9 @@ impl State {
             config,
             size,
             render_pipeline,
-            clear_color,
+            view_uniform,
+            view_buffer,
+            view_bind_group,
         }
     }
 
@@ -177,6 +245,24 @@ impl State {
 
     pub fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
         if new_size.width > 0 && new_size.height > 0 {
+            let scale = (
+                new_size.width as f32 / self.size.width as f32,
+                new_size.height as f32 / self.size.height as f32,
+            );
+
+            let diff = (scale.0 - 1., scale.1 - 1.);
+            // offset
+            let new_min = (-diff.0 / 2., -diff.1 / 2.);
+            let new_max = (1. + (diff.0 / 2.), 1. + (diff.1 / 2.));
+            //project
+            let new_min = self.project_cords(new_min);
+            let new_max = self.project_cords(new_max);
+
+            self.view_uniform.x_min = new_min.0;
+            self.view_uniform.y_min = new_min.1;
+            self.view_uniform.x_max = new_max.0;
+            self.view_uniform.y_max = new_max.1;
+
             self.size = new_size;
             self.config.width = new_size.width;
             self.config.height = new_size.height;
@@ -184,37 +270,42 @@ impl State {
         }
     }
 
+    fn project_cords(&self, pos: (f32, f32)) -> (f32, f32) {
+        // Ranges from 0-1
+        let x_size = self.view_uniform.x_max - self.view_uniform.x_min;
+        let y_size = self.view_uniform.y_max - self.view_uniform.y_min;
+        let size = (x_size, y_size);
+
+        let off = (self.view_uniform.x_min, self.view_uniform.y_min);
+
+        let proj = ((pos.1 * size.0) + off.0, (pos.1 * size.1) + off.1);
+
+        return proj;
+    }
+
     #[allow(dead_code)]
     fn input(&mut self, event: &WindowEvent) -> bool {
         match event {
-            WindowEvent::CursorMoved {
-                device_id: _,
-                position,
-                #[allow(deprecated)]
-                modifiers,
-            } => {
-                let new_color: wgpu::Color;
-                let r = position.x / self.size.width as f64;
-                let b = position.y / self.size.height as f64;
-
-                if modifiers.alt() {
-                    new_color = wgpu::Color {
-                        r,
-                        g: 0.3,
-                        b,
-                        a: 1.0,
-                    }
-                } else {
-                    new_color = wgpu::Color {
-                        r,
-                        g: 0.8,
-                        b,
-                        a: 1.0,
-                    }
+            WindowEvent::MouseWheel { delta, .. } => match delta {
+                MouseScrollDelta::PixelDelta(_delta) => {
+                    println!("Scrolled");
                 }
+                MouseScrollDelta::LineDelta(x, y) => {
+                    let diff = y * 0.1;
 
-                self.clear_color = new_color;
-            }
+                    // offset
+                    let new_min = (diff, diff);
+                    let new_max = (1. - diff, 1. - diff);
+                    //project
+                    let new_min = self.project_cords(new_min);
+                    let new_max = self.project_cords(new_max);
+
+                    self.view_uniform.x_min = new_min.0;
+                    self.view_uniform.y_min = new_min.1;
+                    self.view_uniform.x_max = new_max.0;
+                    self.view_uniform.y_max = new_max.1;
+                }
+            },
             _ => {}
         }
 
@@ -222,7 +313,11 @@ impl State {
     }
 
     fn update(&mut self) {
-        // Nope
+        self.queue.write_buffer(
+            &self.view_buffer,
+            0,
+            bytemuck::cast_slice(&[self.view_uniform]),
+        );
     }
 
     fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
@@ -239,21 +334,6 @@ impl State {
             });
 
         {
-            /*
-            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                label: Some("Render Pass"),
-                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                    view: &view,
-                    resolve_target: None,
-                    ops: wgpu::Operations {
-                        load: wgpu::LoadOp::Clear(self.clear_color),
-                        store: true,
-                    },
-                })],
-                depth_stencil_attachment: None,
-            });
-            */
-            // 1.
             let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
                 label: Some("Render Pass"),
                 color_attachments: &[
@@ -275,8 +355,8 @@ impl State {
                 depth_stencil_attachment: None,
             });
 
-            // NEW!
             render_pass.set_pipeline(&self.render_pipeline); // 2.
+            render_pass.set_bind_group(0, &self.view_bind_group, &[]);
             render_pass.draw(0..6, 0..1); // 3.
         }
 
@@ -319,6 +399,9 @@ pub async fn run() {
             }
             WindowEvent::CursorMoved { .. } => {
                 // let _ = state.input(event);
+            }
+            WindowEvent::MouseWheel { .. } => {
+                let _ = state.input(event);
             }
 
             _ => {}
--- src/shader.wgsl
+++ src/shader.wgsl
@@ -24,26 +24,22 @@ fn vs_main(
     if (in_vertex_index == 0u) {
         let vert = quadVertices[0];
         out.clip_position = vec4<f32>(vert.x, vert.y, 0.0, 1.0);
-    }
-    if (in_vertex_index == 1u) {
+    } else  if (in_vertex_index == 1u) {
         let vert = quadVertices[1];
         out.clip_position = vec4<f32>(vert.x, vert.y, 0.0, 1.0);
-    }
-    if (in_vertex_index == 2u) {
+    } else if (in_vertex_index == 2u) {
         let vert = quadVertices[2];
         out.clip_position = vec4<f32>(vert.x, vert.y, 0.0, 1.0);
-    }
+    } else 
 
     // tri 2
     if (in_vertex_index == 3u) {
         let vert = quadVertices[2];
         out.clip_position = vec4<f32>(vert.x, vert.y, 0.0, 1.0);
-    }
-    if (in_vertex_index == 4u) {
+    } else if (in_vertex_index == 4u) {
         let vert = quadVertices[1];
         out.clip_position = vec4<f32>(vert.x, vert.y, 0.0, 1.0);
-    }
-    if (in_vertex_index == 5u) {
+    } else if (in_vertex_index == 5u) {
         let vert = quadVertices[3];
         out.clip_position = vec4<f32>(vert.x, vert.y, 0.0, 1.0);
     }
@@ -59,6 +55,18 @@ fn vs_main(
 
 
 // Fragment shader
+struct ViewUniform {
+    dpi: f32,
+    radius: f32,
+    max_iterations: u32,
+    x_min: f32,
+    x_max: f32,
+    y_min: f32,
+    y_max: f32,
+}
+
+@group(0) @binding(0) // 1.
+var<uniform> view: ViewUniform;
 
 @fragment
 fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
@@ -66,9 +74,9 @@ fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
 
     let iter: u32 = mandelbrot(uv);
 
-    let p: f32 = 1. / (pow(f32(iter), 0.5) + 1.);
+    let p: f32 = 1. / (pow(f32(iter), 0.7) + 1.);
     
-    let col = vec3<f32>(p, p,p);
+    let col = vec3<f32>(p, .1 , .1);
 
     //let col = (in.vert_pos + vec3<f32>(1.0,1.0,1.0)) / 2.0;
 
@@ -77,22 +85,37 @@ fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
 
  
 const RADIUS: f32 = 2.0;
-const MAX_ITERATION: u32 = 100u;
+const MAX_ITERATION: u32 = 1000u;
+
+fn project_cords(pos: vec2<f32>) -> vec2<f32> {
+    // Ranges from 0-1
+    let norm = (pos + vec2<f32>(1., 1.)) * 0.5;
+
+    let x_size = view.x_max - view.x_min;
+    let y_size = view.y_max - view.y_min;
+    let size = vec2<f32>(x_size, y_size);
+    
+    let off = vec2<f32>(view.x_min, view.y_min);
+
+    let proj = (norm * size) + off;
+
+    return proj;
+}
 
 fn mandelbrot(pos: vec2<f32>) -> u32 {
-       let p0: vec2<f32> = pos - vec2<f32>(0.5, 0.5);
+    let p0: vec2<f32> = project_cords(pos);
 
-       var p: vec2<f32> = vec2<f32>(0.0, 0.0);
-       var iteration: u32 = 0u;
+    var p: vec2<f32> = vec2<f32>(0.0, 0.0);
+    var iteration: u32 = 0u;
 
-        while (dot(p, p) <= (RADIUS * RADIUS) && (iteration < MAX_ITERATION)) {
-            let xtemp: f32 = p.x * p.x - p.y * p.y + p0.x;
-            p.y = 2. * p.x * p.y + p0.y;
-            p.x = xtemp;
-            iteration += 1u;
-        }
+    while (dot(p, p) <= (RADIUS * RADIUS) && (iteration < MAX_ITERATION)) {
+        let xtemp: f32 = p.x * p.x - p.y * p.y + p0.x;
+        p.y = 2. * p.x * p.y + p0.y;
+        p.x = xtemp;
+        iteration += 1u;
+    }
         
-        return iteration;
+    return iteration;
 }
  
 
